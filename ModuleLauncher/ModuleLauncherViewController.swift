178fab39db562682a518513c2b7dea1855dc7b65
danteC94 <dacanizo94@gmail.com>
2021-02-03 09:16:21 -0300

    Removing unnecessary files and making some clean

--- ModuleLauncher/ModuleLauncherViewController.swift
+++ ModuleLauncher/ModuleLauncherViewController.swift
@@ -12,8 +12,6 @@ class ModuleLauncherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func launchModuleTapped(_ sender: Any) {
@@ -24,15 +22,4 @@ class ModuleLauncherViewController: UIViewController {
 
         _ = ViperModule.init(context: managedContext, navigationController: navVC)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
